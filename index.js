ee9570ae22d8c31ea791cecac2b523528566d0d6
Sodikov Dilshod <dilshod200151@gmail.com>
2025-08-08 16:21:03 +0500

    added new

--- index.js
+++ index.js
@@ -1,52 +1,67 @@
-let items = [];
+let items =[];
 
-const itemsDiv = document.getElementById("items")
-const input = document.getElementById("itemInput")
-const storageKey = "items";
+const itemsDiv = document.getElementById('items');
+const input = document.getElementById('itemInput');
+const storageKey = 'items';
+
 
 function renderItems() {
     itemsDiv.innerHTML = null;
 
     for (const [idx, item] of Object.entries(items)) {
-        const container = document.createElement("div")
-        container.style.marginBottom = "10px"
+        const container = document.createElement('div');
+        container.style.marginBottom = '10px';
 
-        const text = document.createElement("p")
-        text.style.display = "inline"
-        text.style.marginRight = "10px"
+        const text = document.createElement('p');
+        text.style.display = 'inline';
+        text.style.marginRight = '10px';
         text.textContent = item;
 
-        const button = document.createElement("button")
-        button.textContent = "Delete"
-        button.onclick = () => removeItem(idx)
+        const button = document.createElement('button');
+        button.textContent = 'Delete';
+        button.onclick = () => removeItem(idx);
 
-        container.appendChild(text)
-        container.appendChild(button)
-        itemsDiv.appendChild(container)
+
+        container.appendChild(text);
+        container.appendChild(button);
+        itemsDiv.appendChild(container);
+    }
+    const counter = document.getElementById(`counter`);
+    if(counter) {
+        counter.textContent = `Total tasks: ${items.length}`;
     }
 }
 
-function loadItems() {
-    const stringItems = JSON.stringify(items);
-    localStorage.setItem(storageKey, stringItems)
+
+function loadItems(){
+    const oldItems = localStorage.getItem(storageKey);
+    if (oldItems) items = JSON.parse(oldItems);
+    renderItems();
 }
+
+function saveItems() {
+    const stringItems = JSON.stringify(items);
+    localStorage.setItem(storageKey, stringItems);
+}
+
 
 function addItem() {
     const value = input.value;
-    if(!value){
-        alert("You cannot add an empty item")
-        return
+    if (!value) {
+    alert("You cannot add an empty item.");
+    return;
     }
-    items.push(value)
-    renderItems()
-    input.value = ""
-    saveItems()
+    items.push(value);
+    renderItems();
+    input.value = '';
+    saveItems();
 }
+
 
 function removeItem(idx) {
-    items.splice(idx, 1)
-    renderItems()
-    saveItems()
+    items.splice(idx, 1);
+    renderItems();
+    saveItems();
 }
 
-document.addEventListener("DOMContentLoaded", loadItems)
+document.addEventListener('DOMContentLoaded', loadItems);
